c70216615a0f000a9b8f5e37dc983673ac7f5d41
Manish Kumar <44120764+mcdroid08@users.noreply.github.com>
2020-06-11 22:56:59 +0530

    add icon to app

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,8 +12,10 @@ public class Main extends Application {
     public void start(Stage stage) throws Exception {
         stage.setTitle("Rock paper scissors");
         FXMLLoader loader = new FXMLLoader(getClass().getResource("MainUI.fxml"));
+        stage.getIcons().add(new Image("icon.png"));
+        
         Parent root = null;
-
+        
         try {
             root = loader.load();
         } catch (IOException e) {
